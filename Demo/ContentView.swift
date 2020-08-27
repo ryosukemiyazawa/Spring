0dae289d1bce4ef6e73dd582be024b34ab8c4428
ryosukemiyazawa <ryosukemiyazawa@gmail.com>
2020-08-27 14:02:22 +0900

    add demo target

---
+++ Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Miyazawa on 2020/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Miyazawa on 2020/08/27.
+//
+
+import UIKit
+import Spring
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		
+		let btn = SpringButton()
+		self.view.addSubview(btn)
+		
+	}
+
+
+}
+
